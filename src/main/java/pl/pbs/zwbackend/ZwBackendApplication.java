21696af93d79369e0bef78a6afef92afb6fee0bf
Bartosz Sowinski <pochbar@gmail.com>
2025-03-30 23:38:45 +0200

    initialize Spring Boot project with Gradle setup and PostgreSQL configuration

---
+++ src/main/java/pl/pbs/zwbackend/ZwBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.pbs.zwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pbs/zwbackend/ZwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pbs.zwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
