460c551e7af1a7ddf562de2a31c9f5c71901fb81
Bartosz Sowinski <pochbar@gmail.com>
2025-04-09 15:43:54 +0200

    configure application properties for database and server settings; add JWT dependencies and environment variable support

--- src/main/java/pl/pbs/zwbackend/ZwBackendApplication.java
+++ src/main/java/pl/pbs/zwbackend/ZwBackendApplication.java
@@ -1,13 +1,26 @@
 package pl.pbs.zwbackend;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZwBackendApplication {
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUser;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
     public static void main(String[] args) {
         SpringApplication.run(ZwBackendApplication.class, args);
     }
-
 }
